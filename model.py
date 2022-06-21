f4485f080465cc29cc140cb5d5c091edfc6066fb
hernypeterson <hgpa2018@mymail.pomona.edu>
2022-06-21 14:20:04 -0500

    compare with ss

--- model.py
+++ model.py
@@ -2,6 +2,8 @@ import numpy as np
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 
+# functions:
+
 def fill_hollow(matrix):
     """Sets diagonal entries so each column sums to zero."""
     matrix -= np.diag(matrix.sum(axis=0))
@@ -10,37 +12,54 @@ def row_col_test(matrix):
     """True if all columns and rows sum to zero."""
     return matrix.sum(axis=0) == matrix.sum(axis=1) == 0
 
+# - - - - - - - - - - - - - -
+
+# characterize model:
+
 # reservoir names
-boxes = ("sed", "soil", "surface_ocean", "ocean_biota", "deep_ocean")
+boxes = ["sed", "soil", "surface_ocean", "ocean_biota", "deep_ocean"]
 N = len(boxes)
 
 # reservoir magnitute at steady state (Tg P)
 reservoir_ss = np.array([2e9, 2e5, 2800, 44, 1e5])
+total = reservoir_ss.sum()
 
-# fluxes at steady state (Tg P / y)
+# fluxes at steady state (Tg P /y)
 flux_ss = np.array([[ 0, 18,   0,   0,  2],
                     [20,  0,   0,   0,  0],
                     [ 0,  2,   0, 940, 56],
                     [ 0,  0, 980,   0,  0],
                     [ 0,  0,  18,  40,  0]])
 
-# fill diagonal terms of flux matrix
+# fill diagonal terms of flux_ss matrix
 fill_hollow(flux_ss)
 
-# convert fluxes to linear rate constants
+# flux (Tg P /yr) to linear rate constants (/yr)
 K = flux_ss / reservoir_ss
 
+# constant coeff. linear homogeneous system y' = Ky
 def f(y, t):
     return K @ y
 
-t_int = np.arange(5)
+# initial reservoir conditions
+reservoir_init = np.array([1.8e6, 3e5, 2800, 44, 1.1e5]) # Tg P
 
-solution = odeint(f, reservoir_ss, t_int)
+# scale to match global magnitude
+reservoir_init *= (total / reservoir_init.sum())
 
+# time interval
+t_int = np.arange(1e6) # yr
+
+# solver
+solution = odeint(f, reservoir_init, t_int)
+
+# ss solution for plot comparison
+solution_ss = np.tile(reservoir_ss, len(t_int)).reshape(len(t_int), N)
+
+# plot
 fig, ax = plt.subplots()
-
-ax.plot(t_int, solution,)
-
+ax.plot(solution)
 ax.set_yscale('log')
-
+plt.legend(boxes, loc='upper right')
+ax.plot(solution_ss, "--", linewidth=0.5)
 plt.savefig("figure.pdf")
