f76f844c541a433ee31bfcd187623674c7c93d18
hernypeterson <hgpa2018@mymail.pomona.edu>
2022-06-20 22:01:05 -0500

    replicates steady state

--- model.py
+++ model.py
@@ -2,6 +2,12 @@ import numpy as np
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 
+# functions
+
+def row_col_test(matrix):
+    """True if all columns and rows sum to zero."""
+    return matrix.sum(axis=0) == matrix.sum(axis=1) == 0
+
 # reservoir names
 boxes = ("sed", "soil", "surface_ocean", "ocean_biota", "deep_ocean")
 N = len(boxes)
@@ -17,24 +23,22 @@ flux_ss = np.array([[ 0, 18,   0,   0,  2],
                     [ 0,  0,  18,  40,  0]])
 
 # fill diagonal terms of flux matrix
-flux_ss += np.diag(flux_ss.sum(axis=0))
+flux_ss -= np.diag(flux_ss.sum(axis=0))
 
 # convert fluxes to linear rate constants
-K = (flux_ss.T / reservoir_ss).T
-
-def ss_test(matrix):
-    """True if all columns and rows sum to zero."""
-    return matrix.sum(axis=0) == matrix.sum(axis=1) == 0
+K = flux_ss / reservoir_ss
 
 def f(y, t):
     return K @ y
 
-t = np.arange(100)
+t_int = np.arange(5)
 
-solution = odeint(f, reservoir_ss, t)
+solution = odeint(f, reservoir_ss, t_int)
 
 fig, ax = plt.subplots()
 
-ax.plot(t, solution, linewidth=2.0)
+ax.plot(t_int, solution,)
+
+ax.set_yscale('log')
 
 plt.savefig("figure.pdf")
