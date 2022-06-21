71784dfdc47f2d44b837c34bf918ceb9ae54662f
hernypeterson <hgpa2018@mymail.pomona.edu>
2022-06-20 19:26:44 -0500

    some variables

--- global-p-model.py
+++ global-p-model.py
@@ -1,3 +1,11 @@
 import numpy as np
 
-state = 
+boxes = ("sed", "soil", "surface_ocean", "ocean_biota", "deep_ocean")
+
+N = len(boxes)
+
+state_0 = np.zeros(N)
+
+np.put(state_0, 0, 2000000000)
+
+print(state_0)
